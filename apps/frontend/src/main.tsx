31c043ec00f0abe8125e4914737383540b77d5c3
Pablo Souza <contato@souzapablo.dev.br>
2026-07-03 18:09:55 -0300

    feat(design-system): aggregate tokens and wire into app entry

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
+import "./design-system/tokens/index.css";
 import { validateEnv } from "./env";
 
 validateEnv();
